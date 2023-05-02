068333a86814a11dc48d140303b5a2842943ecfb
Quantum-Codes <87054411+Quantum-Codes@users.noreply.github.com>
2023-05-02 12:01:59 +0000

    uh

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import Flask, redirect, render_template, request 
 import mysql.connector, os
-
+import bcrypt
+#REMAKE HASHING
 app = Flask('app')
-"""
+
 db = mysql.connector.connect( 
   host = os.environ["Host"],
   user = os.environ["User"],
   password = os.environ["Pass"],
-  database = os.environ["User"]
+  database = "testdb"
 )
 sql = db.cursor()
 
@@ -18,7 +19,21 @@ def result():
 def execute(query):
   sql.execute(query)
   result()
-"""
+
+def hashit(password):
+  return bcrypt.hashpw(password.encode("utf-8"))
+  
+def compareit(hashed, password):
+  return hash.check_password_hash(hashed, password)
+
+"""CREATE TABLE Users (
+id varchar(64) UNIQUE,
+username varchar(25),
+display varchar(25),
+password varchar(30),
+bio TEXT,
+about TEXT
+);"""
 
 @app.route('/')
 def mainpage():
@@ -37,6 +52,12 @@ def loginpage():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signuppage():
+  if request.method == "POST":
+    if not (request.form["username"] and request.form["password"]):
+      return render_template("login.html", mode = "signup", error = "Username or password field was left empty")
+    username = request.form["username"].strip()
+    sql.execute("INSERT INTO Users (id, username, display, password) VALUES (UUID_SHORT(), %s, %s)", params=(username, username, hashit(request.form["password"])))
+    db.commit()
   return render_template("login.html", mode = "signup", error = False)
 
 @app.errorhandler(404)
